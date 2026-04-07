4323415a4045e61584fad32c53b0a0fcf090e8e7
이건 <zkdlu@woowahan.com>
2026-04-07 18:16:37 +0900

    일급컬렉션 추가

---
+++ src/main/java/persistence/PersistenceCacheManager.java
@@ -0,0 +1,25 @@
+package persistence;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PersistenceCacheManager {
+    private final Map<Class<?>, Map<Object, Object>> cache;
+
+    public PersistenceCacheManager() {
+        this.cache = new LinkedHashMap<>();;
+    }
+
+    public <T> T get(Class<T> entityClass, Object key) {
+        return cache.get(entityClass) != null ? (T) cache.get(entityClass).get(key) : null;
+    }
+
+    public <T> void save(Class<T> entityClass, Object key, T entity) {
+        Map<Object, Object> idObjectMap = cache.get(entityClass);
+        if (idObjectMap == null) {
+            idObjectMap = new LinkedHashMap<>();
+            cache.put(entityClass, idObjectMap);
+        }
+        idObjectMap.put(key, entity);
+    }
+}
--- src/main/java/persistence/PersistenceContext.java
+++ src/main/java/persistence/PersistenceContext.java
@@ -4,22 +4,21 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PersistenceContext {
+
+    private final PersistenceCacheManager cacheManager;
     private final Map<Class<?>, Map<Object, Object>> cache;
 
     public PersistenceContext() {
         this.cache = new LinkedHashMap<>();
+        this.cacheManager = new PersistenceCacheManager();
     }
 
     public <T> T get(Class<T> entityClass, Object key) {
-        return cache.get(entityClass) != null ? (T) cache.get(entityClass).get(key) : null;
+        return cacheManager.get(entityClass, key);
+
     }
 
     public <T> void save(Class<T> entityClass, Object key, T entity) {
-        Map<Object, Object> idObjectMap = cache.get(entityClass);
-        if (idObjectMap == null) {
-            idObjectMap = new LinkedHashMap<>();
-            cache.put(entityClass, idObjectMap);
-        }
-        idObjectMap.put(key, entity);
+        cacheManager.save(entityClass, key, entity);
     }
 }
