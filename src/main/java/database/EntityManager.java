e1550a855798b7d1d692b5b583dfec45c14e7fab
권시연 <yeon@woowahan.com>
2026-04-07 17:30:39 +0900

    feat: EntityManagerTest 통과하도록 수정

--- src/main/java/database/EntityManager.java
+++ src/main/java/database/EntityManager.java
@@ -1,23 +1,37 @@
 package database;
 
+
 import java.sql.Connection;
 
 public class EntityManager {
+
     private Connection connection;
+    private Transaction transaction;
+    private PersistenceContext persistenceContext;
 
     public EntityManager(Connection connection) {
         this.connection = connection;
+        this.transaction = new Transaction(connection);
+        this.persistenceContext = new PersistenceContext();
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Connection getConnection() {
         return connection;
     }
 
-    public void close(){
-        try {
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public PersistenceContext getPersistenceContext() {
+        return persistenceContext;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
     }
 }
---
+++ src/test/java/database/EntityManagerTest.java
@@ -0,0 +1,63 @@
+package database;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class EntityManagerTest {
+
+    @Test
+    void EntityManager는_Connection을_관리한다() throws SQLException {
+        Connection conn = createConnection();
+
+        EntityManager em = new EntityManager(conn);
+
+        assertNotNull(em);
+        assertFalse(conn.isClosed());
+
+        em.close();
+
+        assertTrue(conn.isClosed());
+    }
+
+    @Test
+    void EntityManager는_PersistenceContext를_생성한다() {
+        Connection conn = createConnection();
+
+        EntityManager em = new EntityManager(conn);
+
+        assertNotNull(em.getPersistenceContext());
+    }
+
+    @Test
+    void EntityManager는_Transaction을_관리한다() throws SQLException {
+
+        Connection conn = createConnection();
+        EntityManager em = new EntityManager(conn);
+
+        // When: Transaction 시작
+        em.getTransaction().begin();
+
+        assertFalse(conn.getAutoCommit());
+
+        // When: Transaction 종료
+        em.getTransaction().commit();
+
+        assertTrue(conn.getAutoCommit());
+
+        em.close();
+    }
+
+    private Connection createConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:h2:mem:test", "sa", "");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
